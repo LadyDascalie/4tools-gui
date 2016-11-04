1b6b0f5419727e5bedda770b9173a387b4db9867
Benjamin Cable <cable.benjamin@gmail.com>
2016-11-04 17:00:00 +0000

    Added some test assets
    Signed-off-by: Benjamin Cable <cable.benjamin@gmail.com>

--- js/user.js
+++ js/user.js
@@ -1,13 +1,50 @@
 const {shell} = require('electron');
 
 const remote = require('electron').remote;
+const {dialog} = require('electron').remote;
 const app = remote.app;
 
 const spawn = require('child_process').spawn;
 const path = require('path');
 
-var fs = require('fs');
+let fs = require('fs');
+let folderName = '4tools_downloads';
+
 let fourTools;
+let rank;
+
+let filePath = `${app.getPath('home')}/${folderName}`;
+
+// Make this shit recurse later when I'm not busy!
+fs.readdir(filePath, (err, files) => {
+	for (let i = 0; i < files.length; i++) {
+        fs.lstat(`${filePath}/${files[i]}`, (e, f) => {
+			if (f.isDirectory()) {
+				console.log(f);
+			}
+		});
+	}
+});
+
+function scrollToBotton() {
+	let res = document.getElementById('result');
+	res.scrollTop = res.scrollHeight;
+}
+
+function generateRandomKey() {
+	rank = spawn('bin/rank');
+	let res = document.getElementById("result");
+
+	rank.stdout.on('data', (data) => {
+		res.innerText += data;
+		scrollToBotton();
+	});
+}
+
+function clearConsole() {
+	let res = document.getElementById('result');
+	res.innerHTML = '';
+}
 
 function getUserInfo(url, subfolder) {
 	var btn = document.getElementById("dlButton");
@@ -17,9 +54,9 @@ function getUserInfo(url, subfolder) {
 	}
 
 	if (subfolder === "") {
-		fourTools = spawn('bin/4tools', ['-u', url])
+		fourTools = spawn('bin/4tools', ['-u', url]);
 	} else {
-		fourTools = spawn('bin/4tools', ['-u', url, '-f', subfolder])
+		fourTools = spawn('bin/4tools', ['-u', url, '-f', subfolder]);
 	}
 
 	addSpinner();
@@ -28,8 +65,15 @@ function getUserInfo(url, subfolder) {
 
 
 	fourTools.stdout.on('data', (data) => {
-		var d = document.getElementById('result');
+		let d = document.getElementById('result');
+		let b = document.getElementById('main');
+
+		let s = String(data);
+
+		b.style.backgroundImage = `url(${s})`;
+
 		d.innerText += data;
+		scrollToBotton();
 	});
 
 	fourTools.stderr.on('data', (data) => {
@@ -45,7 +89,7 @@ function getUserInfo(url, subfolder) {
 }
 
 function showMe() {
-	shell.showItemInFolder(`${app.getPath('home')}/4tools_downloads`)
+	shell.showItemInFolder(`${app.getPath('home')}/${folderName}`);
 }
 
 function addSpinner() {
@@ -63,4 +107,20 @@ function removeSpinner() {
 	s = document.getElementById("spinner");
 
 	p.removeChild(s)
+}
+
+function pickFile() {
+	var sub = document.getElementById("subFolderName");
+
+	dialog.showOpenDialog({"properties": ['openFile', 'openDirectory', 'createDirectory'], "defaultPath": `${app.getPath('home')}/${folderName}`}, function(file) {
+        if (file == undefined) {
+			return false;
+		}
+
+		var f = file[0];
+		var f = f.split("/");
+		var lastMember = f.length-1;
+
+        sub.value = f[lastMember];
+	});
 }
