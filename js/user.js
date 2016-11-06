092cffbc05c49967eef93829991fbaceb237e853
Benjamin Cable <cable.benjamin@gmail.com>
2016-11-06 16:05:40 +0000

    Added new styles and improved app behaviour
    Signed-off-by: Benjamin Cable <cable.benjamin@gmail.com>

--- js/user.js
+++ js/user.js
@@ -13,27 +13,34 @@ let folderName = '4tools_downloads';
 let fourTools;
 let rank;
 
-let filePath = `${app.getPath('home')}/${folderName}`;
+// // let form = document.getElementById("form");
+// let res = document.getElementById("result");
+
+// let filePath = `${app.getPath('home')}/${folderName}`;
 
 // Make this shit recurse later when I'm not busy!
-fs.readdir(filePath, (err, files) => {
-	for (let i = 0; i < files.length; i++) {
-        fs.lstat(`${filePath}/${files[i]}`, (e, f) => {
-			if (f.isDirectory()) {
-				console.log(f);
-			}
-		});
-	}
-});
+// fs.readdir(filePath, (err, files) => {
+// 	for (let i = 0; i < files.length; i++) {
+//         fs.lstat(`${filePath}/${files[i]}`, (e, f) => {
+// 			if (f.isDirectory()) {
+// 				console.log(f);
+// 			}
+// 		});
+// 	}
+// });
 
+/**
+ * Scroll down to the bottom of the console view
+ */
 function scrollToBotton() {
-	let res = document.getElementById('result');
 	res.scrollTop = res.scrollHeight;
 }
 
+/**
+ * Run rank, not useful, only there for testing
+ */
 function generateRandomKey() {
 	rank = spawn('bin/rank');
-	let res = document.getElementById("result");
 
 	rank.stdout.on('data', (data) => {
 		res.innerText += data;
@@ -41,11 +48,19 @@ function generateRandomKey() {
 	});
 }
 
+/**
+ * Clears the console pane
+ */
 function clearConsole() {
-	let res = document.getElementById('result');
 	res.innerHTML = '';
 }
 
+/**
+ * Retrieve the thread url and sub folder name provided by the user
+ * @param url
+ * @param subfolder
+ * @returns {boolean}
+ */
 function getUserInfo(url, subfolder) {
 	var btn = document.getElementById("dlButton");
 
@@ -63,17 +78,10 @@ function getUserInfo(url, subfolder) {
 	btn.disabled = true;
 	btn.innerText = "Downloading...";
 
-
 	fourTools.stdout.on('data', (data) => {
-		let d = document.getElementById('result');
-		let b = document.getElementById('main');
-
-		let s = String(data);
-
-		b.style.backgroundImage = `url(${s})`;
-
-		d.innerText += data;
-		scrollToBotton();
+		// let b = document.getElementById('main');
+		// // res.innerText += data;
+		// scrollToBotton();
 	});
 
 	fourTools.stderr.on('data', (data) => {
@@ -85,30 +93,59 @@ function getUserInfo(url, subfolder) {
 		removeSpinner();
 		btn.disabled = false;
 		btn.innerText = 'Download';
+        addSuccess();
 	});
 }
 
-function showMe() {
+/**
+ * Open the download folder
+ */
+function openDownloadFolder() {
 	shell.showItemInFolder(`${app.getPath('home')}/${folderName}`);
 }
 
+/**
+ * Adds a success button once the download is complete
+ */
+function addSuccess() {
+    let form = document.getElementById("form");
+
+	let el = document.createElement('div');
+	el.classList = "button success";
+	el.id = "success";
+	el.innerText = "Done!";
+
+	form.appendChild(el);
+}
+
+/**
+ * Adds the spinner while download is underway
+ */
 function addSpinner() {
-	var p = document.getElementById("leftPane");
+	let wrapper = document.getElementById("spinnerWrapper");
 
 	var s = document.createElement('div');
 	s.classList += "loader";
 	s.id = "spinner";
 
-	p.appendChild(s)
+    wrapper.style.display = "block";
+	wrapper.appendChild(s)
 }
 
+/**
+ * Remove the spinner once download is complete
+ */
 function removeSpinner() {
-	p = document.getElementById("leftPane");
-	s = document.getElementById("spinner");
+	let wrapper = document.getElementById("spinnerWrapper");
+	let s = document.getElementById("spinner");
 
-	p.removeChild(s)
+	wrapper.removeChild(s);
+	wrapper.style.display = "none";
 }
 
+/**
+ * Open the file picker view on the default download path
+ */
 function pickFile() {
 	var sub = document.getElementById("subFolderName");
 
--- main.js
+++ main.js
@@ -9,10 +9,10 @@ let win;
 function createWindow() {
     // Create the browser window.
     win = new BrowserWindow({
-        width: 1000,
-        height: 800,
+        width: 450,
+        height: 450,
         titleBarStyle: 'hidden',
-        icon: 'logo.png',
+        icon: 'logo.png'
     });
 
     // and load the index.html of the app.
